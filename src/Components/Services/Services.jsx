8db65ca19712c823f8e5d9d4e019c6a6271b5672
krishnaprasad <krishnaprasad8369@gmail.com>
2024-11-27 04:53:10 +0530

    footer complted

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -1,7 +1,10 @@
 import React, { useEffect } from "react";
 import gsap from "gsap";
 import { BsCursorFill } from "react-icons/bs";
+import { useGSAP } from "@gsap/react";
 const Services = () => {
+
+
   useEffect(() => {
     const rotatedTextElements = document.querySelectorAll(".rotated-text");
 
@@ -16,7 +19,7 @@ const Services = () => {
       elem.innerHTML = clutter;
 
       gsap.from(elem.querySelectorAll("span"), {
-        y: 50,
+        x: 50,
         filter: "blur",
         opacity: 0,
         duration: 0.5,
@@ -27,31 +30,60 @@ const Services = () => {
 
     return () => gsap.killTweensOf(rotatedTextElements);
   }, []);
+  useGSAP(()=>{
+    gsap.from('.header-title',{
+        y:50,
+        opacity:0,
+        delay:0.8,
+        duration:0.7
+    })
+    gsap.from('.banner', {
+        width: 0,
+        duration: 1,
+        delay: 1,
+        scrollTrigger: {
+          trigger: '.banner',
+          start: 'top 80%', 
+          end: 'bottom 20%', 
+          scrub: true, 
+          markers: true, 
+      
+        
+     
+        }
+      });
+  })
 
   return (
     <>
       <main className="py-10">
-        <div className="flex justify-center  flex-wrap-reverse mt-[-2rem] items-center">
-          <div className="flex space-x-4 items-center">
-            <h6 className="rotated-text transform rotate-[-90deg] md:w-[9rem] w-[15rem] text-[12px] sm:text-[15px] font-medium">
-              OUR SERVICES
-            </h6>
-            <hr className="w-px bg-gray-300 h-36" />
-            <h2 className="font-grotesk font-semibold sm:text-[55px] text-[38px] z-40">
-              We craft indelible digital experiences via the best services.
-            </h2>
-          </div>
-          <div className=" ">
-            <img
-              src="/Assets/banner.jpg"
-              alt="Banner"
-              className="w-[1400px] "
-            />
-          </div>
-        </div>
+      <div className="lg:relative flex lg:flex-row justify-center items-center overflow-hidden mt-[-2rem]">
+  {/* Image Section */}
+  <div className="lg:relative flex flex-col-reverse lg:flex-row justify-between w-full  md:w-[80%] ml-0 lg:ml-[18.8rem]">
+    {/* Text Section Overlaid on Image */}
+    <div className="lg:absolute top-[30%] left-[-20%] flex items-center bg-transparent  text-left">
+      <h6 className="rotated-text transform rotate-[-90deg] w-[10rem]   lg:mr-[-2rem] text-[12px] text-center sm:text-[15px] font-medium text-black">
+        OUR SERVICES
+      </h6>
+      <hr className="w-[1px] bg-gray-300 h-[10rem] mx-2" />
+      <h2 className="header-title font-grotesk font-bold  sm:text-[45px] lg:text-[55px] leading-tight text-[30px] bg-transparent text-black">
+        We craft indelible <br /> digital experiences <br /> via the best services.
+      </h2>
+    </div>
+    <div className="flex justify-center w-full"> 
+ 
+      <img
+        src="/Assets/banner.jpg"
+        alt="Banner"
+        className="banner w-[100%] h-auto md:h-[500px]  lg:h-[550px] object-cover md:ml-[5rem] lg:ml-[10rem]" // Set width and allow height to auto
+      />
+    </div>
+  </div>
+</div>
+
         <section className="w-[100%] md:mt-[3rem] mt-[3rem] flex justify-center flex-col items-center">
-          <div className="w-[90%] flex lg:flex-row flex-col justify-center items-center">
-            <div className="w-[90%] lg:w-[900px]">
+          <div className="solution-sec w-[90%] flex lg:flex-row flex-col justify-center items-center">
+            <div className="solution w-[90%] lg:w-[900px]">
               <h2 className="text-[50px] sm:text-[80px] md:text-[100px]  md:text-start text-center  font-grotesk font-semibold">
                 Digital solution
               </h2>
